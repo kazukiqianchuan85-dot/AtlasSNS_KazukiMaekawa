f3cbee6cfab5c3e8343465a424c887f6bfe5251b
和輝 <kazukiqianchuan85@gmail.com>
2025-09-10 11:21:39 +0900

    共通ヘッダー&サイドバーの項目番号01,02,03まで完了

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,13 +1,29 @@
         <div id="head">
-            <h1><a><img src="images/atlas.png"></a></h1>
-            <div id="">
-                <div id="">
+            <h1><a href="{{ url('/top') }}"><img src="images/atlas.png"></a></h1>
+
+            <div class="nav-menu">
+                <div class="user-info">
                     <p>〇〇さん</p>
+                    <button class="accordion-header">
+                        <span class="arrow">▼</span>
+                    </button>
                 </div>
-                <ul>
-                    <li><a href="">ホーム</a></li>
-                    <li><a href="">プロフィール</a></li>
-                    <li><a href="">ログアウト</a></li>
-                </ul>
+
+                <div class="accordion-content">
+                    <ul>
+                        <li><a href="{{ url('/top') }}">ホーム</a></li>
+                        <li><a href="{{ url('/profile') }}">プロフィール編集</a></li>
+                        <li>
+                            <a href="{{ url('/logout') }}"
+                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                ログアウト
+                            </a>
+                            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
+                </div>
             </div>
+
         </div>
